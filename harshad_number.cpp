af02ff541d2f3a25c3c60266004fcd7b75b8aa4c
AshutoshDash1999 <dashashutosh1999@gmail.com>
2022-01-20 23:24:49 +0530

    finding harshad number

---
+++ harshad_number.cpp
@@ -0,0 +1,24 @@
+// Program to determine whether a given number is a Harshad number
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num = 0;
+    cin >>num;
+    int sumOfNum = 0;
+
+    while (num!=0){
+        int rem = num%10;
+        sumOfNum += rem;
+        num = num/10;
+    }
+
+    if (num%sumOfNum == 0){
+        cout << "Its Harshad Number" <<endl;
+    } else {
+        cout << "Its not Harshad Number" <<endl;
+    }
+
+
+    return 0;
+}
